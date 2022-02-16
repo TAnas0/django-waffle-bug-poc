1b780dc3738d5d5281a64733c074be5d66b12c1a
Anas <anastiour@gmail.com>
2022-02-16 04:01:45 +0100

    Expose Users list endpoint

--- djangowafflebug/urls.py
+++ djangowafflebug/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djangowafflebug.app.views import UserViewSet
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', UserViewSet.as_view({'get': 'list'})),
 ]
