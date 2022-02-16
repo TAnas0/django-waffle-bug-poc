84f091ea568b96e3ce6ffb811d3396c188c525d4
Anas <anastiour@gmail.com>
2022-02-16 03:54:30 +0100

    Define UserViewset and UserSerializer

--- djangowafflebug/app/views.py
+++ djangowafflebug/app/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
+from waffle.mixins import WaffleFlagMixin
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework import viewsets
+
+
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=100)
+
+
+class UserViewSet(WaffleFlagMixin, viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing Users.
+    """
+    waffle_flag = "my_flag"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = []
